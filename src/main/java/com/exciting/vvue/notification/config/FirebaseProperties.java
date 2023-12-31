18dd6574f04891a2bfb033f26b29336e7c027d4d
Eundms <dms011221@gmail.com>
2023-12-31 15:24:51 +0900

    fix: firebase

--- src/main/java/com/exciting/vvue/notification/config/FirebaseProperties.java
+++ src/main/java/com/exciting/vvue/notification/config/FirebaseProperties.java
@@ -1,14 +1,37 @@
 package com.exciting.vvue.notification.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix = "cloud.gcp.firebase")
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
 @Getter
 @Setter
 public class FirebaseProperties {
+
+    @Value("${cloud.gcp.firebase.service-account}")
     private Resource serviceAccount;
 
+    public GoogleCredentials getGoogleCredentials() throws IOException {
+        try (InputStream is = serviceAccount.getInputStream()) {
+            return GoogleCredentials.fromStream(is);
+        }
+    }
+
+    public FirebaseApp getFirebaseApp() throws IOException {
+        return FirebaseApp.initializeApp(
+                FirebaseOptions.builder()
+                        .setCredentials(getGoogleCredentials())
+                        .build()
+        );
+    }
 }
